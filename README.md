bffd2de2d4a2819f240ac5bf00a99baca9e7ca4b
wangge <wangge@163.com>
2024-06-20 10:29:59 +0800

    feat: ignore

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # websockets-chat-room
 
 ## 介绍
-- 基于[socket.io](https://socket.io/) + [express](https://expressjs.com/)搭建的聊天室
+- 基于[socket.io](https://socket.io/) + [express框架](https://expressjs.com/)搭建的聊天室
 
-## 启动
+## 启动项目
 ```js
 node server.js
 ```
--- server.js
+++ server.js
@@ -1,33 +1,32 @@
-var express = require('express');
-var socket = require('socket.io'); 
+let express = require("express");
+let socket = require("socket.io");
 
-var app = express();
-var server = app.listen(4000, function(){
-    console.log('listening for requests on port 4000,');
+let app = express();
+let server = app.listen(4000, function () {
+  console.log("websocket server is running  on port 4000");
 });
 
 // 托管静态资源
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // 创建socket服务,
-var io = socket(server);
+let io = socket(server);
 
 // 监听客户端连接的
-io.on('connection', (socket) => {
-    console.log('connection', socket.id);
-    // 监听客户端发生送的聊天事件
-    socket.on('chat', function(data){
-        console.log('chat',data);
-        // 推送chat事件给所有连接者
-        io.sockets.emit('chat', data);
-    });
+io.on("connection", (socket) => {
+  console.log("connection", socket.id);
+  // 监听客户端发生送的聊天事件
+  socket.on("chat", function (data) {
+    console.log("chat", data);
+    // 推送chat事件给所有连接者
+    io.sockets.emit("chat", data);
+  });
 
-    socket.on('typing', function(username){
-        
-        console.log('server typing' + username)
-        // io.sockets.emit('typing', username);  // 向所有客户端推送
+  socket.on("typing", function (username) {
+    console.log("server typing" + username);
+    // io.sockets.emit('typing', username);  // 向所有客户端推送
 
-        // 向所有连接的客户端-发生广播通知
-        socket.broadcast.emit('typing', username); // 向所有客户端推送(排除自己)
-    });
+    // 向所有连接的客户端-发生广播通知
+    socket.broadcast.emit("typing", username); // 向所有客户端推送(排除自己)
+  });
 });
